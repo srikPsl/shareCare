ff79ad985b340c7ef345daacded81f8d5f0d746d
gundubos <srikanth.gunduboyina@cdk.com>
2019-07-27 11:37:48 +0530

    Basic Infra

---
+++ src/main/java/com/honeywell/demo/ShareCareApplication.java
@@ -0,0 +1,13 @@
+package com.honeywell.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareCareApplication.class, args);
+	}
+
+}
