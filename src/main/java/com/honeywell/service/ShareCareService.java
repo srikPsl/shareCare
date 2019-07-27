836ffac663b5cd51544049b99768c654a1f978d1
santoshnayak48 <47916833+santoshnayak48@users.noreply.github.com>
2019-07-27 12:44:43 +0530

    Add files via upload

---
+++ src/main/java/com/honeywell/service/ShareCareService.java
@@ -0,0 +1,17 @@
+package com.honeywell.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.honeywell.model.MapUserGroupMessage;
+
+public class ShareCareService {
+	 public boolean createMessage(MapUserGroupMessage msg){
+		 return true;
+		 
+	 }
+	 
+	 public List<MapUserGroupMessage> getMessages(){
+		 return new ArrayList<MapUserGroupMessage>();
+	 }
+}
